1bd3f7c8a1a48ba27603b074baf247963a697edd
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-07 22:21:00 -0800

    (fix) Add key through env var
    The app now uses the API

--- controllers/image.js
+++ controllers/image.js
@@ -1,8 +1,9 @@
 const Clarifai = require('clarifai');
+const { API_KEY } = require('../keys');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+  apiKey: API_KEY
 });
 
 const handleApiCall = (req, res) => {
@@ -17,12 +18,12 @@ const handleApiCall = (req, res) => {
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
 }
 
 module.exports = {
